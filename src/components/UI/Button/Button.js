b45370cdd906847228772897502387fada79f0dd
Naweed <naweedx1@gmail.com>
2025-08-30 19:53:43 +0430

    U

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -28,6 +28,7 @@ import styles from "./Button.module.css";
 //   }
 // `;
 
+
 const Button = (props) => {
   return (
     <button type={props.type} className={styles.button} onClick={props.onClick}>
